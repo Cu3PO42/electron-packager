1c28aff31c4e4a87d138f327efacdb3085168938
Copper Phosphate <cu3po42@gmail.com>
2017-11-19 20:05:18 +0100

    Prune with respect to server dependencies

--- prune.js
+++ prune.js
@@ -3,6 +3,7 @@
 const child = require('mz/child_process')
 const debug = require('debug')('electron-packager')
 const Walker = require('pruner').Walker
+const fs = require('fs-extra')
 
 const knownPackageManagers = ['npm', 'cnpm', 'yarn']
 
@@ -31,6 +32,24 @@ function pruneModules (opts, appPath) {
 
     if (command) {
       debug(`Pruning modules via: ${command}`)
+      try {
+        const pkgPath = `${appPath}/package.json`
+        const pkg = require(pkgPath)
+        const oldDeps = pkg.dependencies
+        const newDeps = {}
+        if (Array.isArray(pkg.serverDependencies)) {
+          for (const dep of pkg.serverDependencies) {
+            newDeps[dep] = pkg.dependencies[dep]
+          }
+          pkg.dependencies = newDeps
+          return fs.writeFile(pkgPath, JSON.stringify(pkg), 'utf-8')
+          .then(() => child.exec(command, { cwd: appPath }))
+          .then(() => {
+            pkg.dependencies = oldDeps;
+            return fs.writeFile(pkgPath, JSON.stringify(pkg, null, 4), 'utf-8');
+          })
+        }
+      } catch (e) {}
       return child.exec(command, { cwd: appPath })
     } else {
       return Promise.reject(new Error(`Unknown package manager "${packageManager}". Known package managers: ${knownPackageManagers.join(', ')}`))
